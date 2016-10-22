ec4a1510324be3b7cc5bea8c56add57ae014552a
Arman Sahakyan <arman.sahakyan.dev@gmail.com>
2016-10-22 22:19:05 +0400

    added relationship between comboboxes

--- ToolsSelector/ViewModels/Actions.cs
+++ ToolsSelector/ViewModels/Actions.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,8 +10,36 @@ using ToolsSelector.Models;
 
 namespace ToolsSelector.ViewModels
 {
-    public class  Actions
+    public class  Actions /*: INotifyPropertyChanged*/
     {
-        public List<Tool> Tools { get; set; }
+        //public List<Tool> Tools { get; set; }
+
+        //public Actions()
+        //{
+        //    this.G = new List<Tool>();
+        //}
+
+        public List<Tool> X
+        {
+            get;
+            private set;
+        }
+
+        //private Country _selectedCountry
+        //{
+        //    get
+        //    {
+        //        return _selectedCountry;
+        //    }
+        //    set
+        //    {
+        //        _selectedCountry = value;
+        //        /* WARNING: The following code causes the CollectionChanged 
+        //             event to get fired multiple times! */
+        //        this.Cities.Clear();
+        //        foreach (City city in _selectedCountry.Cities)
+        //            this.Cities.Add(city);
+        //    }
+        //}
     }
 }
--- ToolsSelector/Views/MainWindow.xaml.cs
+++ ToolsSelector/Views/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -13,30 +14,66 @@ namespace ToolsSelector
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class  MainWindow :  Window
+    public partial class  MainWindow :  Window,INotifyPropertyChanged
     {
         string jsonFile = File.ReadAllText(@"Resources\Json.json");
 
-        //public Tool SelectedTool { get
-        //    {
-        //        return null;
-
-        //    }
-        //    set
-        //    {
-        //        int a = 0;
-        //    }
-
-        //        }
-
         public List<Tool> x { get; set; }
+        public List<Tool> backup { get; set; }
+             
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        Tool _SelectedTool = null;
+
+        public Tool SelectedTool
+        {
+            get
+            {
+                return _SelectedTool;
+            }
+            set
+            {
+                backup = x;
+
+                if (_SelectedTool != value)
+                {
+                    _SelectedTool = value;
+                    var t = x.Where(x => x.Category == SelectedTool.Category).ToList();
+                    x = new List<Tool>(t);
+                    RaisePropertyChanged("x");
+                }
+            }
+        }
+
+
 
         public MainWindow()
         {
             InitializeComponent();
             x = JsonConvert.DeserializeObject<List<Tool>>(jsonFile);
+            backup = new List<Tool>();
             this.DataContext = this;
         }
+
+        private void RaisePropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+      
+
+        //private void categoryCombo_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+        //{
+        //    nameCombo.ItemsSource = null;
+
+        //    nameCombo.Items.Add("tool");
+
+        //    nameCombo.Items.Refresh();
+        //}
 
     }
 }
